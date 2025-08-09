98564c7983f431a33efb36a1b22aeb7a6d944fa1
jy00566722 <jy00566723@gmail.com>
2025-08-09 23:35:44 +0800

    good

--- extension/wxt.config.js
+++ extension/wxt.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'wxt';
+import vue from '@wxt-dev/module-vue';
 import path from 'node:path';
 
+// See https://wxt.dev/api/config.html
 export default defineConfig({
-  version: "0.0.2",
+  modules: ['@wxt-dev/module-vue'],
   // 在这里为项目根目录定义一个别名 '@'
   alias: {
     '@': path.resolve(__dirname, './'),
   },
   manifest: {
+    name: "Temu数据分析",
+    short_name: "Temu Interceptor",
+    description: "Temu销量分析，补货分析，条码打印.",
+    version: "0.0.2",
     web_accessible_resources: [
       {
         resources: ['/interceptor.js'],
